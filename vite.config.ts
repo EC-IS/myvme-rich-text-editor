6bb249a130634fa86f84d6a614150b64828aa5c8
Yasmine De Wolf <yasminedewolf@hotmail.com>
2024-04-03 17:07:03 +0200

    added dependencies (prettier, sass, vite-plugin-svgr)

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts(), svgr()],
   build: {
     lib: {
-      entry: './src/components/MyVMERichTextEditor.tsx',
-      name: 'MyVMERichTextEditor',
-      formats: ['es', 'umd'],
-      fileName: (format) => `myvme-rich-text-editor.${format}.js`
+      entry: "./src/components/MyVMERichTextEditor.tsx",
+      name: "MyVMERichTextEditor",
+      formats: ["es", "umd"],
+      fileName: (format) => `myvme-rich-text-editor.${format}.js`,
     },
     // Rollup options if needed
     rollupOptions: {
       // Externalize peer dependencies
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
-})
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});

37c4f8547310c1755b14cae9df55c21e4bdd7f2b
Yasmine De Wolf <yasminedewolf@hotmail.com>
2024-04-11 16:14:39 +0200

    multiple ref demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import MyVMERichTextEditor from "./components/MyVMERichTextEditor";
 // import Toolbar from "./components/Toolbar.tsx";
 
 const App = () => {
-  const editorRef = useRef<HTMLDivElement>(null);
-  const editorContainerRef = useRef<HTMLDivElement>(null);
+  const editorRefs = useRef<any[]>([]);
+  const editorContainerRefs = useRef<any[]>([]);
+
+  const list = [
+    { name: "test1", default: "<p>test1</p>" },
+    { name: "test2", default: "<p>test2</p>" },
+    { name: "test3", default: "<p>test3</p>" },
+  ];
+
+  useEffect(() => {
+    editorRefs.current = Array(list.length)
+      .fill(null)
+      .map(() => React.createRef<HTMLDivElement>());
+    editorContainerRefs.current = Array(list.length)
+      .fill(null)
+      .map(() => React.createRef<HTMLDivElement>());
+  }, []);
+
   return (
     <div className="App">
-      <h2 style={{marginBottom:75}}>Custom Rich Text Editor</h2>
-      <MyVMERichTextEditor
-        defaultValue="<p>Typ hier...</p>"
-        editorRef={editorRef}
-        editorContainerRef={editorContainerRef}
-      />
+      <h2 style={{ marginBottom: 75 }}>Custom Rich Text Editor</h2>
+      <div>
+        {list.map((item, index) => (
+          <div>
+            <MyVMERichTextEditor
+              defaultValue={item.default}
+              editorRef={editorRefs.current[index]}
+              editorContainerRef={editorRefs.current[index]}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
